ea0f952c326ca08a15f50d095dda42c31e270bca
Alyssa <AlyssaRSchaefer@gmail.com>
2024-03-09 12:59:00 -0600

    The bean can move!

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -5,14 +5,14 @@ const k = kaboom()
 k.loadSprite("bean", "sprites/bean.png")
 k.loadSprite("gardenbox", "sprites/carrotgrownbox.png")
 
-const player = k.add([
+const bean = k.add([
 	k.pos(120, 80),
 	k.sprite("bean"),
 	k.area(),
 	{ value: 20 }
 ])
 
-k.add([
+const gardenbox = k.add([
 	k.pos(320, 80),
 	k.sprite("gardenbox"),
 	k.scale(.5),
@@ -20,12 +20,32 @@ k.add([
 	{ value: 20 }
 ])
 
-k.onMouseMove(() => {
-	const { x, y } = mousePos();
-	const angle = Math.atan2(y - player.pos.y, x - player.pos.x);
-	const distance = Math.sqrt((x - player.pos.x)^2 + (y - player.pos.y)^2)
-	player.move(vec2(400 * Math.cos(angle),400 * Math.sin(angle)));
+const SPEED = 300;
 
+/*k.onMouseMove(() => {
+	const { x, y } = mousePos();
+	const angle = Math.atan2(y - bean.pos.y, x - bean.pos.x);
+	const distance = Math.sqrt((x - bean.pos.x)^2 + (y - bean.pos.y)^2)
+	bean.move(vec2(400 * Math.cos(angle),400 * Math.sin(angle)));
+
+})*/
+
+/******** MOVEMENT  ********/
+
+onKeyDown("left", () => {
+	bean.move(-SPEED, 0)
+})
+
+onKeyDown("right", () => {
+	bean.move(SPEED, 0)
+})
+
+onKeyDown("up", () => {
+	bean.move(0, -SPEED)
+})
+
+onKeyDown("down", () => {
+	bean.move(0, SPEED)
 })
 
 k.onCollide("bean", "gardenbox", (bean, gardenbox) => {
